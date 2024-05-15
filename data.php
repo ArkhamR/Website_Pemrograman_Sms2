aa77873cac8430ec3d795e7fbd74d4b46054342a
ArkhamR <rahmandika.putra.fadillah@gmail.com>
2024-05-16 01:27:18 +0800

    first commit

---
+++ data.php
@@ -0,0 +1,46 @@
+<?php
+
+$datadiri = [
+    'nama' => 'Rahman Dika',
+    'tgl_lahir' =>'13 - 04 - 2004',
+];
+
+$pendidikan = [
+    [
+        'jenjang' => 'SD',
+        'pendidikan' => 'SDN 1 Karang Asem',
+        'tahun_lulus' => '2012',
+        'no_ijazah' => 'xxx-xx.xx'
+
+    ],
+    [
+        'jenjang' => 'SMP',
+        'pendidikan' => 'SMP 1 Babakan Madang',
+        'tahun_lulus' => '2018',
+        'no_ijazah' => 'xxx-xx.xx'
+
+    ],
+    [
+        'jenjang' => 'SMA',
+        'pendidikan' => 'SMAN 1 Babakan Madang',
+        'tahun_lulus' => '2022',
+        'no_ijazah' => 'xxx-xx.xx'
+
+    ],
+
+];
+
+$organisasi = [
+[
+    'nama' => 'Laskar Pelangi',
+    'jabatan' => 'Ketua',
+    'tahun_gabung' => '2023',
+    'tahun_keluar' => 'saat ini'
+    ],
+[
+    'nama' => 'Pramuka',
+    'jabatan' => 'Wakil',
+    'tahun_gabung' => '1945',
+    'tahun_keluar' => '2000'
+    ],
+];
